04faf7502c7d2e8e70a5b16cb0fcabba7dfe335e
Brownpaper55 <michaelawuahh@gmail.com>
2023-02-19 05:24:09 -0800

    combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 	int ones = 0;
 	int tens = 0;
 
-	for (tens = '0'; tens <= '0'; tens++;)/*prints tens digits*/
+	for (tens = '0'; tens <= '0'; tens++)/*prints tens digits*/
 	{
 		for (ones = '0'; ones <= '0'; ones++)/*print ones digits*/
 		{
