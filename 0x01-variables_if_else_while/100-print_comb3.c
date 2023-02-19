f6f0a3b577a951c59a53656f4b765d0d7560403e
Brownpaper55 <michaelawuahh@gmail.com>
2023-02-19 05:11:39 -0800

    print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - returns the combination of two digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ones = 0;
+	int tens = 0;
+
+	for (tens = '0'; tens <= '0', tens++;)/*prints tens digits*/
+	{
+		for (ones = '0'; ones <= '0'; ones++)/*print ones digits*/
+		{
+			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*adds comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
