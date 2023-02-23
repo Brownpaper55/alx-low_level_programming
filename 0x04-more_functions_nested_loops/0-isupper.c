018fbac96cc03d3edbb5f54f61a73f063af1154e
Brownpaper55 <michaelawuahh@gmail.com>
2023-02-23 14:44:50 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase letters
+ *@x: the value to be checked
+ *Return: return 1 for uppercases else 0
+ */
+
+int _isupper(int x);
+{
+
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
